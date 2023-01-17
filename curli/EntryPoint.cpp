6751b9a94785587a520fbfef37d0e9ed3a8b7a1f
Alper Şahıstan <alpersahistan@gmail.com>
2023-01-16 22:12:12 -0700

    Adjust directories

--- curli/EntryPoint.cpp
+++ curli/EntryPoint.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include <GL/freeglut.h>
 #include <glm/glm.hpp>
+#include <GL/freeglut.h>
 
 int main(int argc, char const *argv[])
 {
@@ -10,14 +10,13 @@ int main(int argc, char const *argv[])
     glutInitWindowPosition(100, 100);
     glutCreateWindow("CuRLI");
     glutDisplayFunc([]() {
-        glClearColor(0.1f, 0.2f, 1.0f, 1.0f);
+        glClearColor(37.f/255.f, 77.f/255.f, 141.f/255.f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
         glutSwapBuffers();
     });
     glutIdleFunc([]() {
         glutPostRedisplay();
     });
-
     glutMainLoop();
     return 0;
 }
